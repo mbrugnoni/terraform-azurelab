331c1620a08c293023ebb6c5b59bead3a0d9bd13
mbrugnoni <michael.brugnoni@gmail.com>
2022-01-16 00:23:58 -0500

    update

--- main.tf
+++ main.tf
@@ -14,7 +14,27 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg-mbrugnon" {
-  name      = "rg-mbrugnon"
+resource "azurerm_resource_group" "rg-mbrugnon-lab" {
+  name      = "rg-mbrugnon-lab"
   location  = var.resource_group_location
+}
+
+resource "azurerm_network_security_group" "mbrugnonlab-nsg" {
+  name                = "mbrugnonlab-nsg"
+  location            = var.resource_group_location
+  resource_group_name = "rg-mbrugnon-lab"
+}
+
+resource "azurerm_virtual_network" "mbrugnonlab-vnet" {
+  name                = "mbrugnonlab-vnet"
+  location            = var.resource_group_location
+  resource_group_name = "rg-mbrugnon-lab"
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "mbrugnonlab-snet1"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.mbrugnonlab-nsg.id
+  }
+
 }
